e897728da380d7ca2e91982a376bf457458dd530
frankiesanjana <francesca.sanjana@gmail.com>
2022-09-03 17:15:57 +0000

    Add main game loop to game.js file

---
+++ game.js
@@ -0,0 +1,29 @@
+// set up game loop
+
+let lastRenderTime = 0;
+
+// how many times the snake moves per second
+const SNAKE_SPEED = 2;
+
+function main(currentTime) {
+    // following line tells us 'when can I animate my next frame?'
+    window.requestAnimationFrame(main);
+    const secondsSinceLastRender = (currentTime - lastRenderTime)/1000;
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+    
+    console.log('Render');
+    lastRenderTime = currentTime;
+
+    update();
+    draw();
+}
+
+window.requestAnimationFrame(main);
+
+function update() {
+
+}
+
+function draw() {
+    
+}
